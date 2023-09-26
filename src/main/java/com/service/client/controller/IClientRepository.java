138b9e3c651a82133d5c1c80ce5fb0b08066c572
Javier Luján <javier.lujan.121@outlook.es>
2023-09-26 12:42:39 -0500

    Client Repository

---
+++ src/main/java/com/service/client/controller/IClientRepository.java
@@ -0,0 +1,7 @@
+package com.service.client.controller;
+
+import com.service.client.model.ClientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IClientRepository extends JpaRepository<ClientEntity, Long> {
+}
