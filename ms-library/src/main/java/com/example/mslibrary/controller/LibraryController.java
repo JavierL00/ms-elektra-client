d7180d1290046b357791c882613cc5933b8d2887
Javier Luján <javier.lujan.121@outlook.es>
2023-09-26 23:38:18 -0500

    Handle exception

--- ms-library/src/main/java/com/example/mslibrary/controller/LibraryController.java
+++ ms-library/src/main/java/com/example/mslibrary/controller/LibraryController.java
@@ -1,10 +1,10 @@
 package com.example.mslibrary.controller;
 
 import com.example.mslibrary.controller.dto.request.LibraryIdentifierDtoRequest;
-import com.example.mslibrary.controller.feign.dto.ClientDtoResponse;
 import com.example.mslibrary.controller.dto.response.LibraryDtoResponse;
-import com.example.mslibrary.controller.feign.dto.ClientDtoRequest;
 import com.example.mslibrary.controller.feign.IFeignPort;
+import com.example.mslibrary.controller.feign.dto.ClientDtoRequest;
+import com.example.mslibrary.controller.feign.dto.ClientDtoResponse;
 import com.example.mslibrary.controller.util.CustomUtilClass;
 import com.example.mslibrary.model.LibraryEntity;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityNotFoundException;
 
 @Log4j2
 @RestController
@@ -37,12 +39,18 @@ public class LibraryController {
         }
 
         try {
-            CustomUtilClass customUtilClass = new CustomUtilClass();
             LibraryEntity libraryEntity = libraryRepository.getById(input.getId());
             log.info("MS Elektra Library [getLibraryById] ---> libraryEntity: {}", libraryEntity);
+
             ClientDtoResponse clientDtoResponse = feignPort.getClientById(new ClientDtoRequest(libraryEntity.getAuthorClientId()));
             log.info("MS Elektra Library [getLibraryById] ---> clientDtoResponse: {}", clientDtoResponse);
+
+            CustomUtilClass customUtilClass = new CustomUtilClass();
             return ResponseEntity.ok(customUtilClass.toDtoResponse(libraryEntity, clientDtoResponse));
+        }
+        catch (EntityNotFoundException e) {
+            log.error("MS Elektra Library [getLibraryById] ---> input error: {}", "No se encontró la biblioteca");
+            return ResponseEntity.notFound().build();
         }
         catch (Exception e) {
             log.error("MS Elektra Library [getLibraryById] ---> error: {}", e.getMessage());
